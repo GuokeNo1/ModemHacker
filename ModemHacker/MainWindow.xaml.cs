ed606fc09f8b4c0deff41b52a2586db4614960a7
Guoke <1962908113@qq.com>
2021-10-29 20:48:38 +0800

    Initial commit

---
+++ ModemHacker/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using Microsoft.Win32;
+using System.IO;
+using LibModemBase;
+using System.Reflection;
+
+namespace ModemHacker
+{
+    public partial class MainWindow : Window
+    {
+        List<ModelData> ModelDatas = new List<ModelData>();
+        bool isGetResult = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void FlushDll()
+        {
+            modelList.Items.Clear();
+            ModelDatas.Clear();
+            string[] dlls = Directory.GetFiles($"{AppDomain.CurrentDomain.BaseDirectory}Modems", "*.dll");
+            for (int i = 0; i < dlls.Length; i++)
+            {
+                Assembly dll = Assembly.LoadFrom(dlls[i]);
+                Type[] type = dll.GetTypes();
+                if (type.Length == 1)
+                {
+                    if (type[0].IsSubclassOf(typeof(ModemBase)))
+                        ModelDatas.Add(new ModelData(type[0]));
+                }
+
+            }
+
+            foreach (var data in ModelDatas)
+            {
+                modelList.Items.Add(data);
+            }
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FlushDll();
+        }
+        private void modelList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (((ModelData)modelList.SelectedItem).useUser)
+            {
+                userBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                userBox.Visibility = Visibility.Collapsed;
+            }
+            isGetResult = false;
+            output.Text = $"请先确认自己的光猫型号为\n{modelList.SelectedItem.ToString()}";
+        }
+
+        private void GET_Click(object sender, RoutedEventArgs e)
+        {
+            if (modelList.SelectedIndex > -1) {
+                isGetResult = true;
+                ModelData selectdata = modelList.SelectedItem as ModelData;
+                string username = userBox.Text;
+                string passwd = passwdBox.Text;
+                string host = ipBox.Text;
+                ModemBase modem;
+                if (selectdata.useUser) {
+                    modem = System.Activator.CreateInstance(selectdata.ClassData, host, username, passwd) as ModemBase;
+                }
+                else {
+                    modem = System.Activator.CreateInstance(selectdata.ClassData, host, passwd) as ModemBase;
+                }
+                string info = modem.getModemInfo();
+                output.Text = info;
+            }
+        }
+        private void SAVE_Click(object sender, RoutedEventArgs e)
+        {
+            if (output.Text != "" && isGetResult) {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "文本文档|*.txt";
+                dialog.FileName = ((ModelData)modelList.SelectedItem).name;
+                bool res = (bool)dialog.ShowDialog();
+                if (res)
+                {
+                    File.WriteAllText(dialog.FileName, output.Text);
+                }
+            }
+        }
+
+        private void MenuItem_Flush_Click(object sender, RoutedEventArgs e)
+        {
+            FlushDll();
+        }
+    }
+}
---
+++ ModemHacker/ModelData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModemHacker
+{
+    class ModelData
+    {
+        public string name = "";
+        public Type ClassData;
+        public bool useUser = false;
+        public ModelData(Type data, bool nu = false) {
+            this.ClassData = data;
+            this.name = data.GetField("modem").GetValue("NoName").ToString();
+        }
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
